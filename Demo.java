1a367b32e3e75ec8ab6887a216f1ca0bebb97e7d
boy12345654321 <54659221+boy12345654321@users.noreply.github.com>
2019-08-29 14:21:08 +0800

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,20 @@
+package com.neuedu.demo0826;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> list = new ArrayList<>();
+		list.add(3);
+		list.add(7);
+		list.add(1);
+		
+		for (int num:list) {
+			System.out.println(num);
+		}
+	}
+
+}
